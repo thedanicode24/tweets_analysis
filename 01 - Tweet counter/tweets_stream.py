202138feab4f568b322ee115f50d1e79554221e0
danieldaduyo <96317976+danieldaduyo@users.noreply.github.com>
2021-12-23 18:52:56 +0100

    Add files via upload

---
+++ 01 - Tweet counter/tweets_stream.py
@@ -0,0 +1,47 @@
+import requests
+import requests_oauthlib
+import socket
+import json
+
+CONSUMER_KEY = "Vt1g6Hak0yk8WcbYDZCw2mYfx"
+CONSUMER_SECRET = "0WT2R1QvobhM9ydIMRUZ9VLvAwXgBTrw69hKQa9WGNNkWpb3Wm"
+ACCESS_TOKEN = "526919768-L38DjWdX68qzY9HBhTHCJDgZMHWAFsVyoalcF0F7"
+ACCESS_SECRET = "bcLcmfzJNZUdj1g6kRmyNtmrelotKE1oBMnQFt1rVcLGY"
+
+URL = 'https://stream.twitter.com/1.1/statuses/filter.json'
+
+my_auth = requests_oauthlib.OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
+
+def get_tweets(target):
+    # query_data = [('language', 'it'), ('locations', '-130,-20,100,50'),('track',target)]
+    query_url = URL+"?track="+target
+    print(query_url)
+    response = requests.get(query_url, auth=my_auth, stream=True)
+    return response
+
+
+def send_tweets(response, conn):
+    for line in response.iter_lines():
+        try:
+            full_tweet = json.loads(line)
+            tweet_text = full_tweet['text']
+            print("Tweet Text: " + tweet_text)
+            print ("-----------------------------------------")
+            conn.send((tweet_text + '\n').encode())
+        except Exception as e:
+            print(e)
+
+
+TCP_IP = "localhost"
+TCP_PORT = 9999
+
+target = input("What is the topic you want to monitor? ").lower()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+print("Waiting for TCP connection...")
+conn, addr = s.accept()
+print("Connected! Starting getting tweets...")
+resp = get_tweets(target)
+send_tweets(resp, conn)
